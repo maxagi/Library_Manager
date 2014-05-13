2297f441f301a5341a608dff82ea32e4f72df69d
Max Agi <maxagi.tau@gmail.com>
2014-05-13 10:19:45 +0300

    added example in  main

--- Libary_Manager/main.cpp
+++ Libary_Manager/main.cpp
@@ -1,7 +1,14 @@
 #include "Library_Manager.h"
+#include<iostream>
 //#include <vld.h>
 using namespace std;
 int main(){
+
+
+	Borrower b1("max");
+	Borrower b2("avi");
+
+	cout <<"id of max is "<< b1.getId() <<endl<<"id of avi is " << b2.getId() << endl;
 
 
 	Books_Container books;
@@ -12,5 +19,7 @@ int main(){
 	list<Book*>  booksByYossi = books.findByAuthor("yossi");
 
 
+	int x;
+	cin >> x;
 	return 0;
 }
